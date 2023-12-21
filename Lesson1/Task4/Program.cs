cb2b4d6b4b76be8f022e659793a385a346a90f8f
Rustam <uz_rustam@mail.ru>
2023-12-21 14:29:32 +0500

    add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 6;
+int b = 13;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
